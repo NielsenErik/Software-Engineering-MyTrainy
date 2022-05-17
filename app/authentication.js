462bc989e0a0397cc8e62df48d5e525bd4fb51f5
Erik Nielsen <eriknielsen98@gmail.com>
2022-05-17 10:09:03 +0200

    add Dockerfile for Heroku

--- app/authentication.js
+++ app/authentication.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router();
 const User = require('./models/user');
 const jwt = require('jsonwebtoken'); 
+require('dotenv').config()
 
-router.post('', async function(req, res) {
-	
+
+router.post('', login = async function(req, res) {
 	// find the user
 	let user = await User.findOne({
 		email: req.body.email
 	}).exec();
+	
 	
 	// user not found
 	if (!user) {
@@ -18,7 +20,9 @@ router.post('', async function(req, res) {
 	// check if password matches
 	if (user.password != req.body.password) {
 		res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+		res.redirect('/')
 	}
+	
 	
 	// if user is found and password is right create a token
 	var payload = {
@@ -26,20 +30,31 @@ router.post('', async function(req, res) {
 		id: user._id
 		// other data encrypted in the token	
 	}
+	
 	var options = {
 		expiresIn: 86400 // expires in 24 hours
 	}
 	var token = jwt.sign(payload, process.env.SUPER_SECRET, options);
 
-	res.json({
-		success: true,
-		message: 'Enjoy your token!',
-		token: token,
-		email: user.email,
-		id: user._id,
-		self: "api/v1/" + user._id
-	});
+	res.redirect('/api/v1/user/'+user._id)
 
 });
+
+router.post('', register = async (req, res) => {
+	const { email, password, userType } = req.body;
+  
+	const emailAlreadyExists = await User.findOne({ email });
+	if (emailAlreadyExists) {
+	  throw new CustomError.BadRequestError('Email already exists');
+	}
+  
+	// first registered user is an admin
+	const isFirstAccount = (await User.countDocuments({})) === 0;
+  
+	const user = await User.create({ email, password, userType });
+	const tokenUser = createTokenUser(user);
+	attachCookiesToResponse({ res, user: tokenUser });
+	res.status(StatusCodes.CREATED).json({ user: tokenUser });
+  });
 
 module.exports = router
--- static/script.js
+++ static/script.js
@@ -11,11 +11,11 @@ var loggedUser = {};
  * if it exists, the studentId is used in future calls.
  */
 function login()
-{
+{   
     //get the form object
     var email = document.getElementById("loginEmail").value;
     var password = document.getElementById("loginPassword").value;
-    // console.log(email);
+    
 
     fetch('../api/v1/authentications', {
         method: 'POST',
@@ -37,5 +37,4 @@ function login()
     .catch( error => console.error(error) ); // If there is any error you will catch them here
 
 };
-login();
 

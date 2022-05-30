061535a14615dee3e7157bfad89b263ad68a2822
Erik Nielsen <eriknielsen98@gmail.com>
2022-05-30 14:25:32 +0200

    add begin card.test

--- app/card.test.js
+++ app/card.test.js
@@ -2,3 +2,21 @@ const request = require('supertest');
 const jwt     = require('jsonwebtoken'); // used to create, sign, and verify tokens
 const app     = require('./app');
 const mongoose = require('mongoose');
+
+describe('GET /api/v1/card', () =>{
+    let connection;
+
+    beforeAll( async () => {
+        jest.setTimeout(8000);
+        jest.unmock('mongoose');
+        connection = await  mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
+        console.log('Database connected!');
+    //return connection; // Need to return the Promise db connection?
+    });
+
+    afterAll( () => {
+        mongoose.connection.close(true);
+        console.log("Database connection closed");
+    });
+
+});

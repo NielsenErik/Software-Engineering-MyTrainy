a7cc692ec47f5073ce1c51ddaea329add255fe8e
NicolaGianuzzi <61273196+NicolaGianuzzi@users.noreply.github.com>
2022-06-01 01:53:53 +0200

    PATCH fetch method
    - aggiunto la fetch per la modifica delle schede di allenamento

--- client/src/components/MyCard.js
+++ client/src/components/MyCard.js
@@ -1,6 +1,6 @@
 import React,{useState} from "react";
 
-import { Tab, Card, Row, Col, Form, Container } from "react-bootstrap";
+import { Tab, Card, Row, Col, Form, Container, Button } from "react-bootstrap";
 import { render } from "react-dom";
 
 // Import data
@@ -10,11 +10,42 @@ import MySmallCard from "./MySmallCard";
 const MyCard = ({eventKey, obj, type}) =>{
     // console.log(`obj in my card:`);
     // console.log(obj);
+    const user = JSON.parse(window.localStorage.getItem("user"))
+
+    const updateCard = (e) =>{
+        e.preventDefault()
+        fetch('http://localhost:3000'+obj.self, {
+            method: "PATCH",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({userId: user, title: title, sport: sport, date: date, comment: description}),
+        })
+        .then((resp) => resp.json()) // Transform the data into json
+        .then(function(data){
+            // console.log(data);
+            window.location.reload()
+        })
+    }
+
+    const [description, setDescription] = useState(obj.comment)
+    const [sport, setSport] = useState(obj.sport)
+    const [title, setTitle] = useState(obj.title)
+    const [date, setDate] = useState(obj.date)
+
     return(
             <Tab.Pane eventKey={eventKey}>
                 <Card style={{ width: '100%', height: "90vh", overflowY: "scrool" }}>
                     <Card.Body style={{overflow: "scroll"}}>
-                        <Card.Title>{obj.title}</Card.Title>
+                        <Card.Title className="mb-2 d-flex justify-items-center align-items-center">
+                            Title: 
+                            <Form.Control 
+                                as="textarea"
+                                className="ms-2"
+                                rows={1} 
+                                defaultValue={title}
+                                onChange={(e) => setTitle(e.target.value)}
+                            />
+                            {/* {obj.title} */}
+                        </Card.Title>
                         <hr 
                             style={{
                                 color: "#3A3A3A",
@@ -22,7 +53,28 @@ const MyCard = ({eventKey, obj, type}) =>{
                                 height: "5"
                             }}
                         />
-                        <Card.Subtitle className="mb-2 text-muted">{obj.sport}</Card.Subtitle>
+                        <Card.Subtitle className="mb-2 text-muted d-flex justify-items-center align-items-center">
+                            Sport:
+                            <Form.Control 
+                                as="textarea"
+                                className="ms-2"
+                                rows={1} 
+                                defaultValue={sport}
+                                onChange={(e) => setSport(e.target.value)}
+                            />
+                            {/* {obj.sport} */}
+                        </Card.Subtitle>
+                        <Card.Subtitle className="my-4 text-muted d-flex justify-items-center align-items-center">
+                            Date:
+                            <Form.Control 
+                                as="textarea"
+                                className="ms-2"
+                                rows={1} 
+                                defaultValue={date}
+                                onChange={(e) => setDate(e.target.value)}
+                            />
+                            {/* {obj.sport} */}
+                        </Card.Subtitle>
                         <Row>
                             <Col xs="auto" className="d-flex align-items-center pe-0">
                                 <Form.Label className="mb-0" htmlFor="exampleColorInput">Colore:</Form.Label>
@@ -39,13 +91,10 @@ const MyCard = ({eventKey, obj, type}) =>{
                         <Form>
                             <Form.Label className="mt-3">Descrizione:</Form.Label>
                             <Form.Control 
-                                as="textarea" 
+                                as="textarea"
                                 rows={3} 
-                                defaultValue={obj.comment}
-                                onChange={(e) => {
-                                    // console.log(e.target.value);
-                                    
-                                }}
+                                defaultValue={description}
+                                onChange={(e) => setDescription(e.target.value)}
                             />
                         </Form>
                         <Card.Subtitle className="mt-2 text-muted">
@@ -63,7 +112,17 @@ const MyCard = ({eventKey, obj, type}) =>{
                             )) : ""}
                         </div>
                     </Card.Body>
-
+                    <Card.Footer>
+                        <div className="align-items-right">
+                            <Button 
+                                variant="primary" 
+                                style={{color:"white"}}
+                                onClick={updateCard}
+                            >
+                                    Save
+                            </Button>
+                        </div>
+                    </Card.Footer>
                 </Card>
             </Tab.Pane>        
     )
--- client/src/components/SideRow.js
+++ client/src/components/SideRow.js
@@ -5,18 +5,29 @@ import ReactDOM from 'react-dom'
 
 const SideRow = ({title, obj}) =>{
 
-    const editHandler = () =>{
-        console.log("Edit handler");
-    }
+    // const editHandler = (e) =>{
+    //     e.preventDefault()
+    //     fetch('http://localhost:3000'+obj.self, {
+    //         method: "PATCH",
+    //         headers: { 'Content-Type': 'application/json' },
+    //         body: JSON.stringify(),
+    //     })
+    //     .then((resp) => resp.json()) // Transform the data into json
+    //     .then(function(data){
+
+    //     })
+        
+    // }
 
     const deleteHandler = (e) =>{
         // alert(`title: ${title} id: ${obj.self}`)
-        e.preventDefault();
+        e.preventDefault()
         fetch('http://localhost:3000'+obj.self, {
             method: "DELETE",
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(),
         })
+        .then((resp) => resp.json()) // Transform the data into json
         .then(function(data){
             alert(`Card ${title}:${obj.self} deleted`)
             window.location.reload()
@@ -29,7 +40,7 @@ const SideRow = ({title, obj}) =>{
                 <p>{title}</p>
             </div>
             <div style={{display: "inline-block"}}>
-                <i className="bi bi-pencil-square" style={{cursor: "pointer"}} onClick={editHandler}></i>
+                {/* <i className="bi bi-pencil-square" style={{cursor: "pointer"}} onClick={editHandler}></i> */}
                 <i className="bi bi-trash3" style={{cursor: "pointer"}} onClick={deleteHandler}></i>
             </div>
         </div>

d2794b98b3015ea1c1d5834287c64da27839d49a
NicolaGianuzzi <61273196+NicolaGianuzzi@users.noreply.github.com>
2022-06-03 15:34:57 +0200

    Update MyNavbar.js
    - Aggiunta fetch del profilo in cui restituisce nella navbar le informazioni dell'utente

--- client/src/components/MyNavbar.js
+++ client/src/components/MyNavbar.js
@@ -1,4 +1,4 @@
-import React,{useState} from "react";
+import React,{useState, useEffect} from "react";
 
 import { Navbar, Container,NavDropdown, Nav, Button, Modal } from "react-bootstrap"
 
@@ -7,10 +7,32 @@ import { useNavigate } from "react-router-dom";
 
 const MyNavbar = () =>{
     const navigate = useNavigate();
+    
+    // User info
+    const user = JSON.parse(window.localStorage.getItem("user"))
+    const [email, setEmail] = useState()
+    const [type, setType] = useState()
+
+
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    useEffect(() =>{
+        fetch('http://localhost:3000/api/v1/users/'+user,{
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(),
+        })
+        .then((resp) => resp.json())
+        .then(function(data){
+            // console.log(data)
+            setEmail(data.user.email)
+            setType(data.user.userType)
+        })
+        .catch(error => alert(error))
+    }, [])
 
     const logOutHandler = (e) =>{
         e.preventDefault()
@@ -30,11 +52,26 @@ const MyNavbar = () =>{
                     <Nav.Link href="/courses">Corsi</Nav.Link>
                     <Nav.Link href="/diary">Diario</Nav.Link>
                     <NavDropdown title="Profilo" id="collasible-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <NavDropdown.Item style={{cursor: "text"}}>
+                            {
+                                email
+                                ?
+                                <p>{email}</p>
+                                :
+                                ""
+                            }
+                        </NavDropdown.Item>
+                        <NavDropdown.Item style={{cursor: "text"}}>
+                            {
+                                type
+                                ?
+                                <p>{type}</p>
+                                :
+                                ""
+                            }
+                        </NavDropdown.Item>
                         <NavDropdown.Divider />
-                        <NavDropdown.Item href="/home" onClick={(e) =>{
+                        <NavDropdown.Item href="/home" style={{cursor: "pointer"}} onClick={(e) =>{
                             e.preventDefault()
                             window.localStorage.clear()
                             navigate('/home')
